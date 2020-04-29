b1ad3de1ce8c982fe056551a893bb093eeb68d7d
Bethan James <bethanjames@Bethans-MacBook-Pro.local>
2020-04-29 12:59:21 +0100

    updated public folder

--- public/exercise.js
+++ public/exercise.js
@@ -17,20 +17,18 @@ const newWorkout = document.querySelector(".new-workout")
 let workoutType = null;
 let shouldNavigateAway = false;
 
-async function initExercise() {
-  let workout;
+init();
 
+async function init() {
   if (location.search.split("=")[1] === undefined) {
-    workout = await API.createWorkout()
-    console.log(workout)
+    const workout = await API.getLastWorkout();
+    if (workout) {
+      location.search = "?id=" + workout._id;
+    } else {
+      newWorkout.classList.add("")
+    }
   }
-  if (workout) {
-    location.search = "?id=" + workout._id;
-  }
-
 }
-
-initExercise();
 
 function handleWorkoutTypeChange(event) {
   workoutType = event.target.value;
